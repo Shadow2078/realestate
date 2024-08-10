af8cf851e7c4c1df82a9ed1d333c24a057d6ed23
Shadow2078 <karkisuman2021@gmail.com>
2024-08-10 13:04:24 +0545

    changes

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -23,7 +23,7 @@ const Contact = () => {
       const response = await getContactInfoApi();
       setContactInfo(response.data);
     } catch (error) {
-      toast.error('Failed to fetch contactq information');
+      toast.error('Failed to fetch contactr information');
     }
   };
 
